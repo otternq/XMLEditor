d4cbd6aaee6f62e59d172dd943320677b375c231
otternq <otternq@gmail.com>
2013-04-22 18:38:53 -0700

    adding server

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/app'));
+
+var port = process.env.PORT || 3000;
+
+app.listen(port);
+
+console.log('Server running at http://127.0.0.1:'+port+'/');
+console.log('To stop the server: CTRL+c');
